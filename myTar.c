9846d559984122ab7e0b408fc005d9ef1df072eb
mkong04 <mkong04@calpoly.edu>
2022-02-20 15:06:22 -0800

    n

--- myTar.c
+++ myTar.c
@@ -2,11 +2,31 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#define BLK_SZ 512
+
+
 int main(int argc, char *argv[]){
+    DIR *d;
+    struct dirent *ent;
+    struct stat sb;
 
-  printf("Hello!\n");
+    /* checking usage */
+    if (argc != 4){
+        printf("usage: mytar [ctxvS]f tarfile [ path [ ... ] ]\n");
+        return 0;
+    }
 
-  return 0;
+    /* creating header */
+    make_header(argv[3]);
+
+    return 0;
 }
 
 /* comment by mark */
+void make_header(char *pathname){
+    DIR *d;
+    struct dirent *ent;
+    struct stat sb;
+
+
+}
--- utils.c
+++ utils.c
@@ -1,1 +1,13 @@
 #include "utils.h"
+
+int main(int argc, char *argv[]){
+
+  if (argc != 4){
+    printf("usage: mytar [ctxvS]f tarfile [ path [ ... ] ]\n");
+    return 0;
+  }
+
+  printf("Hello!\n");
+
+  return 0;
+}
