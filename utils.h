1fae0cb924af874604c2bad03c5311c73acbc0cf
mkong04 <mkong04@calpoly.edu>
2022-02-22 13:20:12 -0800

    h file

--- utils.h
+++ utils.h
@@ -77,7 +77,11 @@ void makeDataBlocks(int fin, int fout);
 
 /* mark */ 
 void make_header(int fd, char *pathname);
-
 void write_file(int fd, struct stat sb, char *pathname);
 
 char det_file_type(struct stat sb);
+char *perms(mode_t mode);
+
+int insert_special_character(char *where, size_t size, int32_t val);
+
+uint32_t extract_special_int(char *where, int len);
